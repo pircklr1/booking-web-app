aa6b03c111ab767d59bcedb0218e103f05a38b8e
pircklr1 <rico.pircklen@gmail.com>
2019-08-19 09:56:15 +0300

    creating server structure

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const PORT = 9999;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.listen(PORT, function() {
+  console.log('Server is running on Port: ' + PORT);
+});
