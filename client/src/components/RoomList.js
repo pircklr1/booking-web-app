7d57cac8decf562f9dc705d46815ff3dc62f8767
siniv <sini.valimaa1@gmail.com>
2019-08-26 13:16:13 +0300

    Datepicker siirretty roomlistiin

--- client/src/components/RoomList.js
+++ client/src/components/RoomList.js
@@ -1,19 +1,32 @@
 import React, {Component} from 'react';
-import {Table, Dropdown, Input, Form} from 'semantic-ui-react'
+import {Table, Dropdown, Input, Form, Container} from 'semantic-ui-react'
 import RoomRow from "./RoomRow";
 import moment from 'moment';
 import "./Table.css"
-import DatePickers from "./DatePickers";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import fi from 'date-fns/locale/fi';
 
 //const data = [{name: "huone1"},{name: "huone2"}, {name: "huone3"}, {name:"huone4"}];
 const data = [{name: 1},{name: 2}, {name: 3}, {name:4}, {name:5}, {name:6}, {name:7}];
 // const array = [];
 
 class RoomList extends Component {
-    state = {
-        now: "",
-        rooms :data
-    };
+
+    constructor(props) {
+      super(props);
+      this.state = {
+          now:"",
+        startDate: new Date(),
+          rooms: data
+      };
+      this.handleDateChange = this.handleDateChange.bind(this);
+    }
+    handleDateChange(date) {
+        this.setState({
+            startDate: date
+        });
+    }
 
     // componentDidMount() {
     //     this.setState({now: moment().format('DD-MM-YYYY')})
@@ -37,15 +50,21 @@ class RoomList extends Component {
             <RoomRow date={this.state.now} room={room} key={room.index}/>);
         return (
             <div>
-                {/*<Form style={{marginTop: 20}}>*/}
-                {/*<Form.Group>*/}
-                {/*    <Form.Input>*/}
-                {/*    <DatePickers/>*/}
-                {/*    </Form.Input>*/}
-                {/*    </Form.Group>*/}
-                {/*</Form>*/}
-
-
+                <Container>
+                    {' '}
+                    <Form style={{ marginTop: 20 }}>
+                        <Form.Group>
+                            <Form.Input>
+                                <DatePicker
+                                    dateFormat='dd/MM/yyyy'
+                                    selected={this.state.startDate}
+                                    onChange={this.handleDateChange}
+                                    locale={fi}
+                                />
+                            </Form.Input>
+                        </Form.Group>
+                    </Form>
+                </Container>
                 <Table unstackable color={'blue'} celled definition>
                     <Table.Header>
                         <Table.Row>
