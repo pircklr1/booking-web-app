5645efcfe337fe6be071873484d0844ac123cfd1
lmetsaranta <lennu_metsaranta@hotmail.com>
2019-08-27 14:25:16 +0300

    varaukset näkyy kalenterinäkymässä oikein (alku-loppu)

--- client/src/components/RoomRow.js
+++ client/src/components/RoomRow.js
@@ -11,47 +11,47 @@ class RoomRow extends Component {
     state = {
         bookingData: this.props.bookings,
         combinedDatas: [
-            {available: true, time: '6:00', room: this.props.room.id},
-            {available: true, time: '6:30', room: this.props.room.id},
-            {available: true, time: '7:00', room: this.props.room.id},
-            {available: true, time: '7:30', room: this.props.room.id},
-            {available: true, time: '8:00', room: this.props.room.id},
-            {available: true, time: '8:30', room: this.props.room.id},
-            {available: true, time: '9:00', room: this.props.room.id},
-            {available: true, time: '9:30', room: this.props.room.id},
-            {available: true, time: '10:00', room: this.props.room.id},
-            {available: true, time: '10:30', room: this.props.room.id},
-            {available: true, time: '11:00', room: this.props.room.id},
-            {available: true, time: '11:30', room: this.props.room.id},
-            {available: true, time: '12:00', room: this.props.room.id},
-            {available: true, time: '12:30', room: this.props.room.id},
-            {available: true, time: '13:00', room: this.props.room.id},
-            {available: true, time: '13:30', room: this.props.room.id},
-            {available: true, time: '14:00', room: this.props.room.id},
-            {available: true, time: '14:30', room: this.props.room.id},
-            {available: true, time: '15:00', room: this.props.room.id},
-            {available: true, time: '15:30', room: this.props.room.id},
-            {available: true, time: '16:00', room: this.props.room.id},
-            {available: true, time: '16:30', room: this.props.room.id},
-            {available: true, time: '17:00', room: this.props.room.id},
-            {available: true, time: '17:30', room: this.props.room.id},
-            {available: true, time: '18:00', room: this.props.room.id},
-            {available: true, time: '18:30', room: this.props.room.id},
-            {available: true, time: '19:00', room: this.props.room.id},
-            {available: true, time: '19:30', room: this.props.room.id},
-            {available: true, time: '20:00', room: this.props.room.id},
-            {available: true, time: '20:30', room: this.props.room.id},
-            {available: true, time: '21:00', room: this.props.room.id},
-            {available: true, time: '21:30', room: this.props.room.id}
+            {available: true, time: moment('6:00:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('6:30:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('7:30:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('7:30:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('8:00:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('8:30:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('9:00:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('9:30:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('10:00:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('10:30:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('11:00:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('11:30:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('12:00:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('12:30:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('13:00:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('13:30:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('14:00:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('14:30:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('15:00:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('15:30:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('16:00:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('16:30:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('17:00:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('17:30:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('18:00:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('18:30:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('19:00:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('19:30:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('20:00:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('20:30:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('21:00:00', 'HH:mm:ss'), room: this.props.room.id},
+            {available: true, time: moment('21:30:00', 'HH:mm:ss'), room: this.props.room.id}
         ]
     }
-
 
     compareData() {
         for (var i = 0; i < this.state.combinedDatas.length; i++) {
             for (var j = 0; j < this.state.bookingData.length; j++) {
                 if (
-                    this.state.combinedDatas[i].time === this.state.bookingData[j].startTime.substring(0, 5) &&
+                    this.state.combinedDatas[i].time.isSameOrAfter(moment(this.state.bookingData[j].startTime, 'HH:mm:ss')) &&
+                    this.state.combinedDatas[i].time.isBefore(moment(this.state.bookingData[j].endTime, 'HH:mm:ss')) &&
                     this.state.combinedDatas[i].room === this.state.bookingData[j].roomId
                 ) {
                     this.state.combinedDatas[i].available = false;
