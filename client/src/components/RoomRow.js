cde5fcc74b4f0e895fb44124391ab33c8894fb84
siniv <sini.valimaa1@gmail.com>
2019-08-28 09:10:20 +0300

    kalenterin päivänäkymä toimii melkein

--- client/src/components/RoomRow.js
+++ client/src/components/RoomRow.js
@@ -46,13 +46,26 @@ class RoomRow extends Component {
         ]
     }
 
+    // compareData() {
+    //     for (var i = 0; i < this.state.combinedDatas.length; i++) {
+    //         for (var j = 0; j < this.state.bookingData.length; j++) {
+    //             if (
+    //                 this.state.combinedDatas[i].time.isSameOrAfter(moment(this.state.bookingData[j].startTime, 'HH:mm:ss')) &&
+    //                 this.state.combinedDatas[i].time.isBefore(moment(this.state.bookingData[j].endTime, 'HH:mm:ss')) &&
+    //                 this.state.combinedDatas[i].room === this.state.bookingData[j].roomId
+    //             ) {
+    //                 this.state.combinedDatas[i].available = false;
+    //             }
+    //         }
+    //     }
+    // }
     compareData() {
         for (var i = 0; i < this.state.combinedDatas.length; i++) {
-            for (var j = 0; j < this.state.bookingData.length; j++) {
+            for (var j = 0; j < this.props.bookings.length; j++) {
                 if (
-                    this.state.combinedDatas[i].time.isSameOrAfter(moment(this.state.bookingData[j].startTime, 'HH:mm:ss')) &&
-                    this.state.combinedDatas[i].time.isBefore(moment(this.state.bookingData[j].endTime, 'HH:mm:ss')) &&
-                    this.state.combinedDatas[i].room === this.state.bookingData[j].roomId
+                    this.state.combinedDatas[i].time.isSameOrAfter(moment(this.props.bookings[j].startTime, 'HH:mm:ss')) &&
+                    this.state.combinedDatas[i].time.isBefore(moment(this.props.bookings[j].endTime, 'HH:mm:ss')) &&
+                    this.state.combinedDatas[i].room === this.props.bookings[j].roomId
                 ) {
                     this.state.combinedDatas[i].available = false;
                 }
@@ -61,12 +74,15 @@ class RoomRow extends Component {
     }
     render() {
 
-        if (this.state.bookingData.length > 0) {
+        // if (this.state.bookingData.length > 0) {
+        //     this.compareData();
+        // }
+        if (this.props.bookings.length > 0) {
             this.compareData();
         }
 
-        const allCells = this.state.combinedDatas.map(combinedData => (
-            <RoomCell cellData={combinedData} key={combinedData.index}/>
+        const allCells = this.state.combinedDatas.map((combinedData, index) => (
+            <RoomCell cellData={combinedData} key={index}/>
         ));
         return (
             <Table.Row>
